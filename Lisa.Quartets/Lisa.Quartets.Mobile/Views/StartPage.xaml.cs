c9293a3578b2792ab843bcb3b6d0e709e55f375b
Reverp <contact@reverbs.pw>
2015-10-05 13:42:56 +0200

    Created the base projects.

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/StartPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lisa.Quartets.Mobile
+{
+	public partial class StartPage : ContentPage
+	{
+		public StartPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
