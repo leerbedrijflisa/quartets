a2a6894cdcac93b461a4c2447c57e62347abc41b
peterbas010 <peterbas0102@live.nl>
2015-10-07 10:06:22 +0200

    Added the Yes/No Screen

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/LockScreen.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lisa.Quartets.Mobile
+{
+	public partial class LockScreen : ContentPage
+	{
+		public LockScreen ()
+		{
+			InitializeComponent ();
+			NavigationPage.SetHasNavigationBar(this, false);
+		}
+		public void BackButtonClicked(object sender, EventArgs args)
+		{
+			// Asynchronously removes the top Page from the navigation stack.
+			Navigation.PopAsync();
+		}
+	}
+}
+
---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/YesNoScreen.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lisa.Quartets.Mobile
+{
+	public partial class YesNoScreen : ContentPage
+	{
+		public YesNoScreen ()
+		{
+			InitializeComponent ();
+			NavigationPage.SetHasNavigationBar(this, false);
+		}
+		async public void JaButtonClicked(object sender, EventArgs args)
+		{
+			// Asynchronously adds a Page to the top of the navigation stack.
+			await Navigation.PushAsync(new StartPage());
+		}
+		async public void NeeButtonClicked(object sender, EventArgs args)
+		{
+			await Navigation.PushAsync(new LockScreen());
+		}
+	}
+}
+
