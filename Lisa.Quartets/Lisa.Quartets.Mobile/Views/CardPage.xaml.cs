4922d9076d8c71d9c32dee9242619cfc3461ba6f
Daan Peters <daan.p@live.com>
2015-10-12 11:26:18 +0200

    no message

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/CardPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lisa.Quartets.Mobile
+{
+	public partial class CardPage : ContentPage
+	{
+		public List<string> selectedImages = new List<string>();
+
+		public CardPage ()
+		{
+			InitializeComponent ();
+		}
+
+		void OnImageClick(object sender, EventArgs args) {	
+			var imageSender = (Image)sender;
+
+			string imageId = imageSender.ClassId.ToString ();
+
+			if (selectedImages.Contains(imageId)) {
+				selectedImages.Remove(imageId);
+				imageSender.Opacity = 1;
+			} else {
+				selectedImages.Add (imageId);
+				imageSender.Opacity = 0.5;
+			}
+		}
+	}
+}
+
