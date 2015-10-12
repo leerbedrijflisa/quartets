7a4d8e3022e773179465933187a77affbd6aff39
Daan Peters <daan.p@live.com>
2015-10-12 11:30:31 +0200

    no message

--- Lisa.Quartets/Lisa.Quartets.Mobile/App.cs
+++ Lisa.Quartets/Lisa.Quartets.Mobile/App.cs
@@ -11,7 +11,7 @@ namespace Lisa.Quartets.Mobile
     {
         public App()
         {
-			MainPage = new StartPage();
+			MainPage = new CardPage();
         }
     }
 }
---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Models/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace Lisa.Quartets.Mobile
+{
+	public class Card
+	{
+		public Card ()
+		{
+		}
+
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+
+		public string FileName { get; set; }
+	}
+}
+
