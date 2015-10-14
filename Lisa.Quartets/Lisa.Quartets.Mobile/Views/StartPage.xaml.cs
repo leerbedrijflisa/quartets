fc8bf938bda561f832755db63eed6f5ef0ade5d2
Erwin <Erwin.vorenhout@hotmail.com>
2015-10-14 10:22:55 +0200

    Added card selector

--- Lisa.Quartets/Lisa.Quartets.Mobile/Views/StartPage.xaml.cs
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/StartPage.xaml.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
+using System.Threading.Tasks;
 
 namespace Lisa.Quartets.Mobile
 {
 	public partial class StartPage : ContentPage
 	{
+
+
 		public StartPage()
 		{
-			InitializeComponent();
+			InitializeComponent ();
+			loadimages ();
+		}
+		 async void loadimages()
+		{
+			
+			var tapGestureRecognizer = new TapGestureRecognizer();
+			var stop = false;
+
+			// Als er op de kaart gedrukt word // 
+			tapGestureRecognizer.Tapped += (s, e) => {
+				stop = true;			
+				System.Diagnostics.Debug.WriteLine("Clicked");
+				var imageSender = (Image)s;
+				imageSender.ScaleTo(2);
+			};
+	
+
+			cardimage.GestureRecognizers.Add (tapGestureRecognizer);
+
+				for (int i = 1; i <= 44; i++) 
+				{
+				
+						if (stop == false) 
+						{				
+							await Task.Delay (500);
+							var card = new Image { Aspect = Aspect.AspectFit };
+							card.Source = ImageSource.FromFile ("card" + i + ".jpg");
+							card.IsVisible = true;	
+							cardimage.Source = card.Source;
+						}
+			
+			    }
+				
+
+
 		}
 	}
-}
+
+} 
