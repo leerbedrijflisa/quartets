4ae1ccac0981a6040bb2fb74b558882852e6f2de
William Willing <william@williamwilling.com>
2015-10-14 14:38:06 +0200

    Refactored card carrousel.

--- Lisa.Quartets/Lisa.Quartets.Mobile/Views/StartPage.xaml.cs
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/StartPage.xaml.cs
@@ -7,47 +7,43 @@ namespace Lisa.Quartets.Mobile
 {
 	public partial class StartPage : ContentPage
 	{
-
-
 		public StartPage()
 		{
-			InitializeComponent ();
-			loadimages ();
+			InitializeComponent();
+			RegisterTapRecognizer();
+			ShowImageCarrousel();
 		}
-		 async void loadimages()
+
+		private async void ShowImageCarrousel()
+		{	
+			for (int i = 1; i <= 44; i++)
+			{
+				if (stop == false)
+				{				
+					await Task.Delay(500);
+					var card = new Image { Aspect = Aspect.AspectFit };
+					card.Source = ImageSource.FromFile("card" + i + ".jpg");
+					card.IsVisible = true;	
+					cardimage.Source = card.Source;
+				}
+			}
+		}
+
+		private void RegisterTapRecognizer()
 		{
-			
 			var tapGestureRecognizer = new TapGestureRecognizer();
 			var stop = false;
 
 			// Als er op de kaart gedrukt word // 
-			tapGestureRecognizer.Tapped += (s, e) => {
+			tapGestureRecognizer.Tapped += (s, e) =>
+			{
 				stop = true;			
 				System.Diagnostics.Debug.WriteLine("Clicked");
-				var imageSender = (Image)s;
+				var imageSender = (Image) s;
 				imageSender.ScaleTo(2);
 			};
-	
 
-			cardimage.GestureRecognizers.Add (tapGestureRecognizer);
-
-				for (int i = 1; i <= 44; i++) 
-				{
-				
-						if (stop == false) 
-						{				
-							await Task.Delay (500);
-							var card = new Image { Aspect = Aspect.AspectFit };
-							card.Source = ImageSource.FromFile ("card" + i + ".jpg");
-							card.IsVisible = true;	
-							cardimage.Source = card.Source;
-						}
-			
-			    }
-				
-
-
+			cardimage.GestureRecognizers.Add(tapGestureRecognizer);
 		}
 	}
-
-} 
+}
