524f321e88e5324d5d4f146b747baa65450bc2d2
Erwin <Erwin.vorenhout@hotmail.com>
2015-12-10 10:20:12 +0100

    Added fade animation and slowed timer down.

--- Lisa.Quartets/Lisa.Quartets.Mobile/Views/AskCard.xaml.cs
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/AskCard.xaml.cs
@@ -8,40 +8,47 @@ namespace Lisa.Quartets.Mobile
 {
 	public partial class AskCard : ContentPage
 	{
-		private bool stop;
-	
 		public AskCard()
 		{
-			int id = 0;
-
+			InitializeComponent();
+			InitializeFirstImage();
+			Timer();
+		}
+		public void InitializeFirstImage()
+		{
+			cardimage.Source = ImageSource.FromFile("card1.jpg");
 			var tapGestureRecognizer = new TapGestureRecognizer();
 			cardimage.GestureRecognizers.Add(tapGestureRecognizer);
-
-
-			tapGestureRecognizer.Tapped += (s, e) =>
-			{
-				System.Diagnostics.Debug.WriteLine("Clicked");
-				stop = true;
-			};
-
-			Device.StartTimer (new TimeSpan (0, 0, 0,0,300), () => {
-
-				if(stop == true)
+			tapGestureRecognizer.Tapped += OnCardClick;
+		}
+		private void OnCardClick(object sender, EventArgs args)
+		{
+			_stop = true;
+		}
+		public void Timer(){
+			Device.StartTimer (new TimeSpan (0, 0, 0,2,0), () => {
+				if(_stop == true)
 				{
 					System.Diagnostics.Debug.WriteLine("false");
-					cardimage.Scale = 1.6;
+					cardimage.ScaleTo(1.6);
 					return false;	
 				}
-
-				var card = new Image { Aspect = Aspect.AspectFit };
-				card.Source = ImageSource.FromFile("card" + id + ".jpg");
-				card.IsVisible = true;  
-				cardimage.Source = card.Source;
-				id++;
+				FadeCard(_id);
+				_id++;
+				if (_id > 43)
+				{
+					_id = 0;
+				}
 				return true;
 			});
-
-
 		}
+		public async void FadeCard(int id){
+			await cardimage.FadeTo(0,500);
+			cardimage.Source = ImageSource.FromFile("card" + _id + ".jpg"); 
+			await cardimage.FadeTo(1,500);
+		}
+		private bool _stop;
+		private int _id;
 	}
+
 }
