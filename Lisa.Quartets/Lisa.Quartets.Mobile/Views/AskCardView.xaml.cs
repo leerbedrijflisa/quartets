1ee03245f4181f493204ff711e3d97b2c3be3188
Erwin <Erwin.vorenhout@hotmail.com>
2016-01-13 14:01:02 +0100

    Shuffled askcardview list

--- Lisa.Quartets/Lisa.Quartets.Mobile/Views/AskCardView.xaml.cs
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/AskCardView.xaml.cs
@@ -13,6 +13,7 @@ namespace Lisa.Quartets.Mobile
 		{
 			InitializeComponent();
 			_cards = _database.RetrieveCardsInHand(0);
+			Shuffle(_cards);
 			InitializeFirstImage();
 			Timer();
 		}
@@ -25,6 +26,18 @@ namespace Lisa.Quartets.Mobile
 
 			cardimage.Source = _cards[0].FileName;
 		}
+		public IList<T> Shuffle<T>(IList<T> list) {
+			int n = list.Count;
+			Random rnd = new Random();
+			while (n > 1) {
+				int k = (rnd.Next(0, n) % n);
+				n--;
+				T value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
+			return list;
+		}
 
 		private void OnCardClick(object sender, EventArgs args)
 		{
@@ -35,25 +48,31 @@ namespace Lisa.Quartets.Mobile
 			Device.StartTimer (new TimeSpan (0, 0, 0,2,0), () => {
 				if(_stop == true)
 				{
-					System.Diagnostics.Debug.WriteLine("false");
 					cardimage.ScaleTo(1.6);
 					return false;	
 				}
-				FadeCard(_id);
 				_id++;
 				if (_id >= _cards.Count)
 				{
 					_id = 0;
 				}
+
+
+				FadeCard(_id);
+		
+
+
+
 				return true;
 			});
 		}
-		public async void FadeCard(int id){
-			
+
+		public async void FadeCard(int id){			
 			await cardimage.FadeTo(0,500);
 			cardimage.Source = _cards[id].FileName;
 			await cardimage.FadeTo(1,500);
 		}
+
 		private CardDatabase _database = new CardDatabase();
 		private bool _stop;
 		private List<Card> _cards = new List<Card>();
