1c93288ed584fae6cd90452f55730046af372233
peterbas010 <peterbas0102@live.nl>
2016-01-29 16:00:20 +0100

    rotating slingers

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/QuartetView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lisa.Quartets.Mobile
+{
+	public partial class QuartetView : ContentPage
+	{
+		public QuartetView ()
+		{
+			InitializeComponent ();
+			slingersAnimation ();
+			slingers2Animation ();
+		}
+
+		public async void slingersAnimation()
+		{
+			for (int i = 1; i <= 3; i++)
+			{
+				slingers.Rotation = 10;
+				await Task.Delay(500);
+				slingers.Rotation = 0;
+				await Task.Delay(500);
+				slingers.Rotation = -10;
+				await Task.Delay(500);
+				slingers.Rotation = 0;
+				await Task.Delay(500);
+			}
+		}
+
+		public async void slingers2Animation()
+		{
+			for (int i = 1; i <= 3; i++)
+			{
+				slingers2.Rotation = -10;
+				await Task.Delay(500);
+				slingers2.Rotation = 0;
+				await Task.Delay(500);
+				slingers2.Rotation = 10;
+				await Task.Delay(500);
+				slingers2.Rotation = 0;
+				await Task.Delay(500);
+			}
+		}
+	}
+}
+
