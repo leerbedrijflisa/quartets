e09e4fe3ed993dd6f6281a5a6e19fc65113b0fb6
Angelo Roks <angeloroks@hotmail.com>
2016-03-03 14:07:57 +0100

    Removed some newlines in main activity.cs, and SQLite_Android.cs Added SettingsView

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/SettingsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Lisa.Quartets.Mobile
+{
+	public partial class SettingsView : ContentPage
+	{
+		public SettingsView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
