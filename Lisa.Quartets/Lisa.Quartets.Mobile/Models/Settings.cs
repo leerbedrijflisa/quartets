c2de570d483d6566e8cdc8ad6fdf20b9f0b3c17a
Angelo Roks <angeloroks@hotmail.com>
2016-03-15 14:39:18 +0100

    Added the model for the settings table, applied some minor changes to the settings view, added create for settings table

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Models/Settings.cs
@@ -0,0 +1,11 @@
+﻿using SQLite.Net.Attributes;
+
+namespace Lisa.Quartets.Mobile
+{
+	public class Settings
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public int Delay { get; set; }
+	}
+}
