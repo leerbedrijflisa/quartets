0d910b5a25a7043a879494aca73a6c933e594fac
Lucas Assink <lucas.assink@gmail.com>
2016-03-17 09:23:38 +0100

    animation of the cards spinning into the screen

--- Lisa.Quartets/Lisa.Quartets.Mobile/Views/QuartetView.xaml.cs
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/QuartetView.xaml.cs
@@ -8,36 +8,42 @@ namespace Lisa.Quartets.Mobile
 {
 	public partial class QuartetView : ContentPage
 	{
-        public QuartetView (int category)
+		public QuartetView (int category)
 		{
 			InitializeComponent();
 
-            SetImages(category);
-            Animation();
-            PlaySound();           
+			SetImages(category);
+			Animation();
+			PlaySound();           
 		}
 
-        private void SetImages(int category)
-        {
-            CardLayout cardGrid = new CardLayout();
-            cardGrid.HorizontalOptions = LayoutOptions.CenterAndExpand;
-            cardGrid.VerticalOptions = LayoutOptions.CenterAndExpand;
-            List<Card> quartet =  _database.RetrieveQuartet(category);
+		private double width;
+		private double height;
 
-            foreach (var card in quartet)
-            {
-                CardImage image = new CardImage();
-                image.Source = card.FileName;
-                image.CardId = card.Id;
+		protected override void OnSizeAllocated (double width, double height){
+			base.OnSizeAllocated (width, height);
+			if (width != this.width || height != this.height) {
+				this.width = width;
+				this.height = height;
+				if (width > height) {
+					quartetCards.Orientation = StackOrientation.Horizontal;
+				} else {
+					quartetCards.Orientation = StackOrientation.Vertical;
+				}
+			}
+		}
 
-                image.Scale = 0.8;
+		private void SetImages(int category)
+		{
+			List<Card> quartet =  _database.RetrieveQuartet(category);
 
-                cardGrid.Children.Add(image);
-            }
+				card1.Source = quartet [0].FileName;
+				card2.Source = quartet [1].FileName;
+				card3.Source = quartet [2].FileName;
+				card4.Source = quartet [3].FileName;
 
-            layout.Children.Add(cardGrid);
-        }
-			
+		}
+
 		private void PlaySound() 
 		{
 			DependencyService.Get<IAudio>().PlayFile("clapping");
@@ -45,22 +51,23 @@ namespace Lisa.Quartets.Mobile
 
 		public void Animation()
 		{
+			CardsAnimation ();
 			SlingersAnimation();
 		}
 
 		public void AdvanceToNextPage()
 		{
-            List<Card> cards = _database.RetrieveAskableCards();
-            if (cards.Count == 0)
-            {
-                Navigation.InsertPageBefore(new EmptyHandView(), this);
-            }
-            else
-            {
-                Navigation.InsertPageBefore(new RequestView(), this);
-            }
+			List<Card> cards = _database.RetrieveAskableCards();
+			if (cards.Count == 0)
+			{
+				Navigation.InsertPageBefore(new EmptyHandView(), this);
+			}
+			else
+			{
+				Navigation.InsertPageBefore(new RequestView(), this);
+			}
 
-            Navigation.PopAsync();
+			Navigation.PopAsync();
 		}
 
 		public async void SlingersAnimation()
@@ -87,11 +94,66 @@ namespace Lisa.Quartets.Mobile
 				{
 					AdvanceToNextPage();
 				}
-					
+
 			}
 
 		}
 
-        private CardDatabase _database = new CardDatabase();
+		public async void CardsAnimation()
+		{
+			int x = 0;
+			for (int i = 0; i < 90; i++)
+			{
+				card1.Rotation = card1.Rotation + 16;
+				card1.Scale = card1.Scale + 0.01;
+				x++;
+				await Task.Delay (1);
+				if (x == 30) {
+					Card2Animation ();
+				}
+			}
+		}
+
+		public async void Card2Animation()
+		{
+			int x = 0;
+			for (int i = 0; i < 90; i++)
+			{
+				card2.Rotation = card2.Rotation + 16;
+				card2.Scale = card2.Scale + 0.01;
+				x++;
+				await Task.Delay (1);
+				if (x == 30) {
+					Card3Animation ();
+				}
+			}
+		}
+		public async void Card3Animation()
+		{
+			int x = 0;
+			for (int i = 0; i < 90; i++)
+			{
+				card3.Rotation = card3.Rotation + 16;
+				card3.Scale = card3.Scale + 0.01;
+				x++;
+				await Task.Delay (1);
+				if (x == 30) {
+					Card4Animation ();
+				}
+			}
+		}
+		public async void Card4Animation()
+		{
+			int x = 0;
+			for (int i = 0; i < 90; i++)
+			{
+				card4.Rotation = card4.Rotation + 16;
+				card4.Scale = card4.Scale + 0.01;
+				x++;
+				await Task.Delay (1);
+			}
+		}
+
+		private CardDatabase _database = new CardDatabase();
 	}
 }
