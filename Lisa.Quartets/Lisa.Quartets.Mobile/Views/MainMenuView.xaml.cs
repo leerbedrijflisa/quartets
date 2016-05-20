475c63f6d14b1abc1bdb3aaad1418be11fa84393
Daan Peters <daan.p@live.com>
2016-05-20 10:58:18 +0200

    Added the new main menu

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/MainMenuView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lisa.Quartets.Mobile
+{
+    public partial class MainMenuView : ContentPage
+    {
+        public MainMenuView()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void StartButtonClicked(object sender, EventArgs args)
+        {
+            Navigation.InsertPageBefore(new HandEditorView(typeof(StartView)), this);
+            Navigation.PopAsync();
+        }
+
+        private void OptionsButtonClicked(object sender, EventArgs args)
+        {
+//            Navigation.InsertPageBefore(new , this);
+//            Navigation.PopAsync();
+            DisplayAlert("melding","opties","oké");
+        }
+
+        private void InstructionsButtonClicked(object sender, EventArgs args)
+        {
+//            Navigation.InsertPageBefore(new , this);
+//            Navigation.PopAsync();
+            DisplayAlert("melding","instructies","oké");
+        }
+
+        private void CreditsButtonClicked(object sender, EventArgs args)
+        {
+//            Navigation.InsertPageBefore(new , this);
+//            Navigation.PopAsync();
+            DisplayAlert("melding","credits","oké");
+        }
+    }
+}
+
