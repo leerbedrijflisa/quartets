883dbdbb494f8c6e0be1cff2657a38ef83aa0457
Joost Ronkes Agerbeek <joost@Joosts-MacBook-Pro.local>
2016-05-31 09:45:45 +0200

    You can now swipe and tap to ask a card

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/FancyFrame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Lisa.Quartets
+{
+    public class FancyFrame : Frame
+    {
+        public event EventHandler OnSwipe;
+
+        public void SendSwipe()
+        {
+            var handler = OnSwipe;
+            if(handler!=null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+}
+
