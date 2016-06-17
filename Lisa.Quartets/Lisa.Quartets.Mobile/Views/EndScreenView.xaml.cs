2b74968ba593c7860fa23a653898a07f27ef954e
Angelo Roks <angeloroks@hotmail.com>
2016-06-17 14:24:51 +0200

    Added  the first animation, still requires some minor tweaks.

--- Lisa.Quartets/Lisa.Quartets.Mobile/Views/EndScreenView.xaml.cs
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/EndScreenView.xaml.cs
@@ -16,14 +16,29 @@ namespace Lisa.Quartets.Mobile
 
 		private void OnTap (object sender, EventArgs args)
 		{
-			Debug.WriteLine ("hello");
+			//Debug.WriteLine ("hello");
+			List<Card> quartet = _database.RetrieveNonQuartetCardsInHand ();
+			card1.Source = quartet [0].FileName;
+			TappedLayout.Children.Add (card1);
+			//Debug.WriteLine (card1);
+			int x = 0;
+			for(int i = 0; i < 20; i++){
+				card1.Scale = card1.Scale + 0.01;
+				card1.Rotation = card1.Rotation + 16;
+				x++;
+				if (x == 30)
+				{
+					Debug.WriteLine ("Stop");
+				}
+			}
 		}
 		public void Tapped ()
 		{
 			var tapGestureRecognizer = new TapGestureRecognizer ();
 			tapGestureRecognizer.Tapped += OnTap;
-			TappedStackLayout.GestureRecognizers.Add (tapGestureRecognizer);
+			TappedLayout.GestureRecognizers.Add (tapGestureRecognizer);
 		}
+		private CardDatabase _database = new CardDatabase ();
 	}
 }
 
