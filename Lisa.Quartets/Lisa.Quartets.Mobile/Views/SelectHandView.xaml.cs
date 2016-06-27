1dd2c31c9a7409de8395a60273c070363f3520e3
Daan Peters <daan.p@live.com>
2016-06-27 09:27:40 +0200

    You can now see all the cards that you've added to your hand

--- Lisa.Quartets/Lisa.Quartets.Mobile/Views/SelectHandView.xaml.cs
+++ Lisa.Quartets/Lisa.Quartets.Mobile/Views/SelectHandView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using System.Threading.Tasks;
 
 namespace Lisa.Quartets.Mobile
 {
@@ -22,11 +23,12 @@ namespace Lisa.Quartets.Mobile
             {
                 _selectedCard = _database.RetrieveFromNumber(_input);
 
-                if (_selectedCard != null)
+                if (_selectedCard != null && _selectedCard.IsInHand == 0)
                 {
+                    _cardCount++;
                     ShowCard();
                     SaveSelectedCard();
-                    _cardCount++;
+                    AddToOverview();
                     ToggleButtons(true);
                 }
 
@@ -51,6 +53,10 @@ namespace Lisa.Quartets.Mobile
             _database.CreateOrUpdateCard(_selectedCard);
 
             cardImage.Source = null;
+            selectionOverview.Children.RemoveAt(selectionOverview.Children.Count - 1);
+
+
+            _images.RemoveAt(_images.Count - 1);
 
             _cardCount--;
             if (_cardCount <= 0)
@@ -76,9 +82,56 @@ namespace Lisa.Quartets.Mobile
             doneButton.IsEnabled = isEnabled;
         }
 
+        private async void AddToOverview()
+        {
+            CardImage cardImage = new CardImage
+            {
+                Source = _selectedCard.FileName,
+                TranslationX = Width
+            };
+
+            selectionOverview.Children.Add(cardImage);
+
+            // Create animations for all the images that are already on the screen.
+            List<Task> tasks = new List<Task>();
+            ​
+            for (int i = 0; i < _images.Count; i++)
+            {
+                var image = _images[i];
+                double offset = CalculatePosition(i, _images.Count + 1, 100, Width);
+                tasks.Add(image.TranslateTo(offset, 0, 500, Easing.CubicInOut));
+            }
+            ​
+            // Create animation for the new image.
+            tasks.Add(cardImage.TranslateTo(CalculatePosition(_images.Count, _images.Count + 1, 100, Width), 0, 500, Easing.CubicOut));
+            ​
+            // Store the new image in the list of images already on the screen.
+            _images.Add(cardImage);
+            ​
+            // Run the animations.
+            await Task.WhenAll(tasks.ToArray());
+        }
+
+        private double CalculatePosition(int index, int imageCount, double imageWidth, double containerWidth)
+        {
+            if (imageCount * imageWidth > containerWidth)
+            {
+                double itemWidth = (containerWidth - imageWidth) / imageCount;
+                return index * itemWidth;
+            }
+            else
+            {
+                double center = containerWidth / 2;
+                double indexOffset = index - (imageCount - 1) / 2.0;
+                return center + indexOffset * imageWidth - imageWidth / 2;
+            }
+
+        } 
+
         private string _input;
         private int _cardCount;
         private Card _selectedCard;
         private CardDatabase _database = new CardDatabase();
+        private List<CardImage> _images = new List<CardImage>();
     }
 }
