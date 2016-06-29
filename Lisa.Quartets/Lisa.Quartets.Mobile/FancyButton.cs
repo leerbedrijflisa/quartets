93e814abda9fa8692bd78a70f03fb44e337e07d8
Daan Peters <daan.p@live.com>
2016-06-29 09:40:32 +0200

    Made som appearance changes to the app

---
+++ Lisa.Quartets/Lisa.Quartets.Mobile/FancyButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Lisa.Quartets
+{
+    public class FancyButton : Button
+    {
+        public FancyButton()
+        {
+            BackgroundColor = Color.FromHex("c0edd6");
+            TextColor = Color.White;
+            BorderColor = Color.Black;
+            BorderWidth = 1;
+            BorderRadius = 20;                
+        }
+    }
+}
+
